aa73f5af87808cc434736a930004546b299ac33c
Jubaid Hossain Nahid <72185200+NahidJHN@users.noreply.github.com>
2021-07-03 20:22:39 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// TASK ONE
+var firstName = "Javascript",
+    lastName = "Learner",
+    age = 25;
+
+//Task two
+var x = 55;
+var y = 45;
+var result;
+//  addition
+result = x + y;
+console.log(result); // 100
+// production
+result = x * y;
+console.log(result); // 2475
+
+// multiplication
+result = x - y;
+console.log(result); // 10
+
+//division
+result = x / y;
+console.log(result); //1.22222
+
+console.log((x += y)); // 100
+
+console.log((x -= y)); // 55
+
+console.log((x *= y)); // 2475
+
+console.log((x /= y)); // 55
+
+// all the calculated value store in x variable
+
+//TAST 3 data type
+
+var ength = 16; //number
+
+var Name = "Admin"; //string
+
+x = {
+    firstName: "Javascript",
+    lastName: "Group",
+}; //object
